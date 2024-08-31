df9129ba7e62d7a2ca153b069ce0d56da0c57126
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-08-31 15:54:09 +0530

    mapping spec with ? mapping

--- src/main/java/org/example/introspringwebmvc/controller/Demo.java
+++ src/main/java/org/example/introspringwebmvc/controller/Demo.java
@@ -1,6 +1,7 @@
 package org.example.introspringwebmvc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,13 @@ public class Demo  {
     public String healthTest() {
         return "OK";
     }
+    @PostMapping
+    public String healthTest2() {
+        return "OK POST";
+    }
+    @PostMapping("post")
+    public String healthTest3() {
+        return "OK POST 2";
+    }
+
 }
